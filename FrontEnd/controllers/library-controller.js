3bb705480a94b77899cf5fbd68d792ad8a56e12e
Paul Soderquist <paulsoderquist3@gmail.com>
2016-01-25 13:46:47 -0700

    more

--- FrontEnd/controllers/library-controller.js
+++ FrontEnd/controllers/library-controller.js
@@ -165,7 +165,7 @@ app.controller('libraryController',
 			$http.get('http://104.236.169.62:' + port + '/selectall/media_library')
 			.success(function(data){
 				myFactory.entries.listed = data.json;
-				$scope.entries = myFactory.mediaList;
+				$scope.entries = myFactory.entries;
 			}).error(function()
 			{
 				alert('failure');
