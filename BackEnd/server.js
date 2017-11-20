a689b74c33157dad1f9ad78f932771355394d274
psoder3 <paulsoderquist3@gmail.com>
2017-11-19 23:35:29 -0700

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -147,8 +147,8 @@ app.get('/selectall/:table', function(req,res){
 					checker: '55',
 					q1a: '499275',
 					q1b: '499,275',
-					q2a: '2062',
-					q2b: '2,062'
+					q2a: '4603',
+					q2b: '4,603'
 				});
 				connection.release();
 			});
