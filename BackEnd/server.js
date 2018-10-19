cd28550a16e3c154f29b65943b497797220b5a48
psoder3 <paulsoderquist3@gmail.com>
2018-10-19 15:34:40 -0600

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -209,7 +209,7 @@ function dealUnoDeck(gameData,totalPlayers)
 		}
 	}
 	gameData['Playable_Deck'] = [];
-	gameData['currentTurn'] = 0;
+	gameData['currentTurn'] = -1;
 	gameData['Playable_Deck'].push(gameData['totalCards'][0]);
 	gameData['totalCards'].splice(0, 1);
 }
