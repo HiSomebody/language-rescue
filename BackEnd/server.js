9ed5926527b1afb18b60f67efc2c061cffc3db73
psoder3 <paulsoderquist3@gmail.com>
2018-12-14 13:01:56 -0700

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -147,8 +147,8 @@ app.post('/newSnakeHighScore/:playerName/:score', function(req,res){
 		}
 		highScoresList.push({name: playerName, score: score});
 		
-		highScoresList.sort(function(a, b){return a.score - b.score});
-		highScoresList.reverse();
+		highScoresList.sort(function(a, b){return b.score - a.score});
+		
 		highScoresList = highScoresList.slice(0,10);
 		var toWrite = "";
 		for (var i = 0; i < highScoresList.length; i++)
