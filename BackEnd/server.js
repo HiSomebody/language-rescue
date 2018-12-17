bf3c5fcf6e9a6226d0558d85c70121f0e246e016
psoder3 <paulsoderquist3@gmail.com>
2018-12-17 13:59:07 -0700

    pointed handle to Objshfjcated Snake html

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1542,7 +1542,7 @@ res.sendFile(path.resolve(__dirname + '/../FrontEnd/GAMES/imageSort5x5.html'));
 });
 
 app.get('/snakeHadden', function(req,res){
-res.sendFile(path.resolve(__dirname + '/../FrontEnd/GAMES/snakeHadden.html'));
+res.sendFile(path.resolve(__dirname + '/../FrontEnd/GAMES/snakeHadden_Obfuscated_.html'));
 });
 
 app.get('/sortit4x4', function(req,res){
